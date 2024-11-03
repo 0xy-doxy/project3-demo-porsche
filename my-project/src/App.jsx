7524fc834fb0c2eca88f5d22b8b20a626cbea75c
0xy-doxy <121454689+0xy-doxy@users.noreply.github.com>
2024-11-03 12:23:26 +0530

    Update App.jsx

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   useEffect(() => {
-      axios.get('http://localhost:3001/user', { withCredentials: true })
+      axios.get('https://demo-porsche-backend.onrender.com', { withCredentials: true })
           .then(response => {
               if (response.data.user) {
                   setIsLoggedIn(true);
